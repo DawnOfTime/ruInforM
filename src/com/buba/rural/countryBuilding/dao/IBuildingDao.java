544f09620f11f53b96e66b005f2b18fb0bf730b7
ran <ran@192.168.56.1>
2017-05-11 21:49:21 +0800

    zhr

--- src/com/buba/rural/countryBuilding/dao/IBuildingDao.java
+++ src/com/buba/rural/countryBuilding/dao/IBuildingDao.java
@@ -6,11 +6,11 @@ import com.buba.rural.comm.PageBean_easyui;
 import com.buba.rural.pojo.Country_building;
 
 public interface IBuildingDao {
-	public abstract PageBean_easyui seachbuilding(Country_building countrybuilding,PageBean_easyui pageBean);
+	public abstract Country_building seachbuilding(Country_building countrybuilding);
 
 	public abstract String addaddBuilding(Country_building countrybuilding);
 	public abstract String updated(Country_building[] country);
 	public abstract String deleted(Country_building[] country);
 	public abstract List seachrecord(Country_building countrybuilding);
-	public abstract List seachlishi(Country_building countrybuilding);
+	public abstract PageBean_easyui seachlishi(Country_building countrybuilding, PageBean_easyui pageBean);
 }
--- src/com/buba/rural/countryBuilding/service/IBuildingService.java
+++ src/com/buba/rural/countryBuilding/service/IBuildingService.java
@@ -7,10 +7,12 @@ import com.buba.rural.pojo.Country_building;
 import com.buba.rural.pojo.Country_message;
 
 public interface IBuildingService {
-	public abstract  PageBean_easyui seachbuilding(Country_building countrybuilding,PageBean_easyui pageBean);
+/*	public abstract  PageBean_easyui seachbuilding(Country_building countrybuilding,PageBean_easyui pageBean);*/
 	public abstract String addBuilding(Country_building countrybuilding);
 	public abstract String deleted(Country_building[] country,Country_building countrybuilding);
 	public abstract String updated(Country_building[] country,Country_building countrybuilding);
 	public abstract List seachrecord(Country_building countrybuilding);
-	public abstract List seachlishi(Country_building countrybuilding);
+	public abstract Country_building searchbuilding(Country_building countrybuilding);
+	public abstract PageBean_easyui seachlishi(
+			Country_building countrybuilding, PageBean_easyui pageBean);
 }
